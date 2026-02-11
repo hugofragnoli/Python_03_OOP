320dbfcaa60ad722ed22852c1e38b68a877a9bc1
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-11 20:43:38 +0100

    reprise + docstrings

--- ex00/S1E9.py
+++ ex00/S1E9.py
@@ -1,10 +1,15 @@
 from abc import ABC, abstractmethod
 
 class Character(ABC):
+    """
+    Docstring pour Character
+    """
     def __init__(self, first_name, is_alive=True):
         self.first_name = first_name
         self.is_alive = is_alive
 
 class Stark(Character):
+    """
+    Docstring pour Stark
+    """
     
-
