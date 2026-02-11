eb4734e55cb702f33d29bfb7f650d8c3927793f1
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-11 21:07:49 +0100

    debut class abstraite

--- ex00/S1E9.py
+++ ex00/S1E9.py
@@ -8,8 +8,22 @@ class Character(ABC):
         self.first_name = first_name
         self.is_alive = is_alive
 
+    @abstractmethod
+    def __str__(self):
+        # ici on met rien, on dit a python : je vais donner cette methode
+        # a toutes les classes qui heriteront de celle la (abstr).
+        pass
+
+    def __repr__(self):
+        # repr est destinee au dev. Elle doit etre technique
+        # et permettre de reconstruire lobjet.
+        # utilisee quand on tape le nom la variable dans la
+        # console ou quand on affiche une liste dobjets.
+        pass
+
+
 class Stark(Character):
     """
     Docstring pour Stark
     """
-    
+
