8bbf88e08dfcd2778c4944d9403cec1797c0922c
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-11 21:12:24 +0100

    deplacer commentaires + ajout doc

--- ex00/S1E9.py
+++ ex00/S1E9.py
@@ -1,24 +1,29 @@
 from abc import ABC, abstractmethod
 
+
 class Character(ABC):
     """
-    Docstring pour Character
+    Classe de base abstraite pour tous les personnages.
+    Elle définit le contrat que chaque famille doit suivre.
     """
     def __init__(self, first_name, is_alive=True):
         self.first_name = first_name
         self.is_alive = is_alive
 
+    # le decorateur @abstractmethod oblige les classes enfant
+    # a devoir partager cette methode. Sinon linterpreteur ping
+    # ici on met rien, on dit a python : je vais donner cette methode
+    # a toutes les classes qui heriteront de celle la (abstr).
     @abstractmethod
     def __str__(self):
-        # ici on met rien, on dit a python : je vais donner cette methode
-        # a toutes les classes qui heriteront de celle la (abstr).
         pass
 
+    # repr est destinee au dev. Elle doit etre technique
+    # et permettre de reconstruire lobjet.
+    # utilisee quand on tape le nom la variable dans la
+    # console ou quand on affiche une liste dobjets.
+    @abstractmethod
     def __repr__(self):
-        # repr est destinee au dev. Elle doit etre technique
-        # et permettre de reconstruire lobjet.
-        # utilisee quand on tape le nom la variable dans la
-        # console ou quand on affiche une liste dobjets.
         pass
 
 
