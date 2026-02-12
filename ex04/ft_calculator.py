44ac668eee09fb489d6ab7e8f2803f8d54b55ba5
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-12 18:27:46 +0100

    fin logique calculator

--- ex04/ft_calculator.py
+++ ex04/ft_calculator.py
@@ -5,15 +5,15 @@ class calculator:
     def add_vec(V1: list, V2: list) -> None:
         """Ajoute le scalaire à chaque élément et affiche le vecteur."""
         res = [x + y for x, y in zip(V1, V2)]
-        print(self.vec)
+        print(f"Add vector is : {res}")
 
     def dotproduct(V1: list, V2: list) -> None:
         """Multiplie le scalaire a chaque element et affiche le vecteur"""
         new_vec = [x * y for x, y in zip(V1, V2)]
         a = sum(new_vec)
-        print(a)
+        print(f"Dot product is : {a}")
     
     def sous_vec(V1: list, V2: list) -> None:
         """Multiplie le scalaire a chaque element et affiche le vecteur"""
-        self.vec = [x - y for x, y in zip(V1, V2)]
-        print(self.vec)
+        res = [x - y for x, y in zip(V1, V2)]
+        print(f"Sous vector is : {res}")
