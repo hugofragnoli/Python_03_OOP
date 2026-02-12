f28b636f0505c7a18aeea1aee8e2777e192535f0
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-12 18:26:30 +0100

    maj print et remove self

--- ex04/ft_calculator.py
+++ ex04/ft_calculator.py
@@ -2,18 +2,18 @@ class calculator:
     """Classe pour effectuer des opérations sur des vecteurs sans instance."""
 
     # zip(v1, v2) génère : (1.0, 10.0), (2.0, 20.0), (3.0, 30.0).
-    def add_vec(self, object) -> None:
+    def add_vec(V1: list, V2: list) -> None:
         """Ajoute le scalaire à chaque élément et affiche le vecteur."""
-        self.vec = [x + y for x, y in zip(self.vec, object)]
+        res = [x + y for x, y in zip(V1, V2)]
         print(self.vec)
 
-    def dotproduct(self, object) -> None:
+    def dotproduct(V1: list, V2: list) -> None:
         """Multiplie le scalaire a chaque element et affiche le vecteur"""
-        new_vec = [x * y for x, y in zip(self.vec, object)]
+        new_vec = [x * y for x, y in zip(V1, V2)]
         a = sum(new_vec)
         print(a)
     
-    def sous_vec(self, object) -> None:
+    def sous_vec(V1: list, V2: list) -> None:
         """Multiplie le scalaire a chaque element et affiche le vecteur"""
-        self.vec = [x - y for x, y in zip(self.vec, object)]
+        self.vec = [x - y for x, y in zip(V1, V2)]
         print(self.vec)
